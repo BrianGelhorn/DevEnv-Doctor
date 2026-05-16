3a550b207439eacbdc676d082288fd2add59ec58
Brian Desktop <briangelhorn@gmail.com>
2026-05-16 10:40:22 -0300

    Fixed linting problems and added check_docker_compose_valid_build_or_image function to compose.py

--- src/devenv_doctor/checks/compose.py
+++ src/devenv_doctor/checks/compose.py
@@ -1,20 +1,27 @@
-from devenv_doctor.core import command_output
 from pathlib import Path
-from typing import Optional
-from yaml.scanner import ScannerError
+
 import yaml
+from yaml import YAMLError
+
+from devenv_doctor.core import command_output
+
 
 def check_docker_compose_available() -> tuple[bool, str]:
     exit_code, output = command_output(["docker", "compose", "version"])
-    if  exit_code == 0:
+    if exit_code == 0:
         return True, "Docker Compose is available"
-    
+
     output_lower = output.lower()
 
     if "permission denied" in output_lower:
-        return False, "Docker Compose is installed but your user does not have permission to access it."
-    
+        return (
+            False,
+            "Docker Compose is installed but your user does not have permission "
+            "to access it.",
+        )
+
     return False, output
+
 
 COMPOSE_FILENAMES = (
     "compose.yaml",
@@ -23,10 +30,11 @@ COMPOSE_FILENAMES = (
     "docker-compose.yaml",
 )
 
+
 def find_compose_file(project_path: Path) -> Path | None:
     for filename in COMPOSE_FILENAMES:
         compose_file = project_path / filename
-        
+
         if compose_file.is_file():
             return compose_file
     return None
@@ -38,17 +46,19 @@ def check_docker_compose_file_exists(project_path: Path) -> tuple[bool, str]:
         return False, "No Docker Compose file was found."
     return True, f"Docker Compose file found: {compose_file.name}"
 
-def parse_yaml_file(file_path: Path) -> Optional[dict]:
+
+def parse_yaml_file(file_path: Path) -> dict | None:
     try:
         with file_path.open("r", encoding="utf-8") as file:
             return yaml.safe_load(file) or {}
-    except ScannerError:
+    except YAMLError:
         return None
+
 
 def check_docker_compose_yaml_syntax(project_path: Path) -> tuple[bool, str]:
     compose_file = find_compose_file(project_path)
-    
-    # This should not execute since the check_docker_compose_file_exists function 
+
+    # This should not run since the check_docker_compose_file_exists function
     # should be called before.
     if compose_file is None:
         return False, "No Docker Compose file was found."
@@ -57,26 +67,57 @@ def check_docker_compose_yaml_syntax(project_path: Path) -> tuple[bool, str]:
         return False, "Docker Compose file is empty."
     if parsed_yaml is None:
         return False, "Docker Compose file YAML has syntax errors."
-    return True, "Docker Compose file has valid YAML syntax."   
+    return True, "Docker Compose file has valid YAML syntax."
+
 
 def check_docker_compose_services_section(project_path: Path) -> tuple[bool, str]:
     compose_file = find_compose_file(project_path)
-    
-    # This should not execute since the check_docker_compose_file_exists function 
+
+    # This should not run since the check_docker_compose_file_exists function
     # should be called before.
     if compose_file is None:
         return False, "No Docker Compose file was found."
-    
+
     parsed_yaml = parse_yaml_file(compose_file)
-    
-    # This should not execute since the check_docker_compose_yaml_syntax function 
+
+    # This should not run since the check_docker_compose_yaml_syntax function
     # should be called before.
     if parsed_yaml is None:
         return False, "Docker Compose file YAML has syntax errors."
-    if not "services" in parsed_yaml.keys():
+    if "services" not in parsed_yaml.keys():
         return False, "The services section does not exist."
     if parsed_yaml["services"] is None:
         return False, "The services section is empty."
     if not isinstance(parsed_yaml["services"], dict):
         return False, "The services section must be a YAML object."
     return True, "The services section exists and is not empty."
+
+
+def check_docker_compose_valid_build_or_image(project_path: Path) -> tuple[bool, str]:
+    compose_file = find_compose_file(project_path)
+    # This should not run since the check_docker_compose_file_exists function
+    # should be called before.
+    if compose_file is None:
+        return False, "No Docker Compose file was found."
+
+    parsed_yaml = parse_yaml_file(compose_file)
+
+    # This should not run since the check_docker_compose_yaml_syntax function
+    # should be called before.
+    if parsed_yaml is None:
+        return False, "Docker Compose file YAML has syntax errors."
+    services_with_error = []
+    for service in parsed_yaml["services"].keys():
+        service_keys = parsed_yaml["services"][service].keys()
+        if "build" not in service_keys and "image" not in service_keys:
+            services_with_error.append(service)
+
+    if not len(services_with_error) == 0:
+        output = "The following services have no build or image tag: "
+        for service in services_with_error:
+            output += f"{service}, "
+        # Delete last space and comma and add final dot
+        output = f"{output[0:len(output)-2]}."
+
+        return False, output
+    return True, "All services have either build or image tag."
--- src/devenv_doctor/checks/docker.py
+++ src/devenv_doctor/checks/docker.py
@@ -1,35 +1,41 @@
 from devenv_doctor.core import command_output
+
+
 def check_docker_cli_installed() -> tuple[bool, str]:
     exit_code, output = command_output(["docker", "--version"])
     if exit_code == 0:
         return True, "Docker CLI is installed."
-    
+
     if exit_code == 127:
         return False, "Docker CLI is not installed."
     return False, output
+
 
 def check_docker_daemon_accessible() -> tuple[bool, str]:
     exit_code, output = command_output(["docker", "info"])
     if exit_code == 0:
         return exit_code == 0, "Docker daemon is accessible"
-    
+
     output_lower = output.lower()
 
-    # This should not execute since the check_docker_cli_installed function 
+    # This should not run since the check_docker_cli_installed function
     # should be called before.
     if exit_code == 127:
         return False, output
-    
+
     if exit_code == 124:
-        return False, "Docker Command timed out. Docker may be slow, stuck or not responding."
-    
+        return (
+            False,
+            "Docker Command timed out. Docker may be slow, stuck or not responding.",
+        )
+
     if "permission denied" in output_lower:
         return False, (
             "Docker daemon may be running, "
             "but your user does not have permission to access it."
-            )
+        )
 
     if "cannot connect to the docker daemon" in output_lower:
         return False, "Docker daemon is not running or is not reachable."
-    
+
     return False, output
