d59baee865456fdee27ef0124c0e1f65a9b78e20
Brian Desktop <briangelhorn@gmail.com>
2026-05-16 02:34:30 -0300

    Created core folder with the code for executing a command and getting the output and exit code

---
+++ src/devenv_doctor/core/command.py
@@ -0,0 +1,18 @@
+from subprocess import run, TimeoutExpired
+
+def command_output(command: list[str]) -> tuple[int, str]:
+    try:
+        result = run(command,
+                     capture_output=True,
+                     text=True,
+                     check=False,
+                     timeout=5)
+        if result.returncode == 0:
+            return result.returncode, result.stdout.strip()
+        return result.returncode, result.stderr.strip()
+    except FileNotFoundError as e:
+        # 127 is code in linux for command not found
+        return 127, f"Command not found: {e.filename}"
+    except TimeoutExpired as e:
+        # 124 is code in linux for timeout
+        return 124, f"The program timed out after {e.timeout} seconds"
